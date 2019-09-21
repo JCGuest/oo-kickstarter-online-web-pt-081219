347c6be4ad4162134c14ea887dcbb5a3f637d3ae
John Guest <jcguest90@gmail.com>
2019-09-21 04:30:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer
 attr_reader :name
-attr_accessor :backed_projects
+#attr_accessor :backed_projects
 @@backed_projects = []
 
 def initialize(name)
 @name = name
+@title = title
 @@backed_projects
 end
 
